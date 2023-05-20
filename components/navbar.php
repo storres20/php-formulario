2640d748f0afa6f90089a433504f17759b8ef362
storres20 <sanchotorres777@gmail.com>
2023-05-20 07:31:09 -0500

    Add navbar

---
+++ components/navbar.php
@@ -0,0 +1,7 @@
+<nav>
+    <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="admin.php">Admin</a></li>
+        <li><a href="principal.php">Principal</a></li>
+    </ul>
+</nav>
